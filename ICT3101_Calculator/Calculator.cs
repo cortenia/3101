551828fc87499f9b3a511126ec413e58b4edafcc
cortenia <cortenia87@gmail.com>
2021-10-08 08:37:32 +0800

    .

--- ICT3101_Calculator/Calculator.cs
+++ ICT3101_Calculator/Calculator.cs
@@ -158,7 +158,7 @@ namespace ICT3101_Calculator
             //Dependency------------------------------ 
             //FileReader getTheMagic = new FileReader();
             //---------------------------------------- 
-            string[] magicStrings = getTheMagic.Read(Path.Combine(Environment.CurrentDirectory, @"..\..\..\MagicNumbers.txt"));
+            string[] magicStrings = getTheMagic.Read(Path.Combine(Environment.CurrentDirectory, @"../../../MagicNumbers.txt"));
 
             if ((choice >= 0) && (choice < magicStrings.Length))
             {
